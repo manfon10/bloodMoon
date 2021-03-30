73a74daf7991a71d4cef8834443c8ba837bc2f55
Manuel Fonseca <manueldazafon@gmail.com>
2021-03-30 11:25:28 -0500

    agrego botones para el menu de aministracion.

--- views/administration/administration.php
+++ views/administration/administration.php
@@ -5,7 +5,16 @@
                 <button onclick="redirect('../ajax/common_tabs.php', 'view_user')" id="view_user" value="view_user"><i class="fa fa-user"></i> Usuarios</button>
             </div>
             <div class="buttonsAdministration">
-                <button><i class="fa fa-plus"></i> Editar Reportes</button>
+                <button onclick="redirect('../ajax/common_tabs.php', 'view_category')" id="view_category" value="view_category"><i class="fa fa-tags"></i> Categorias</button>
+            </div>
+            <div class="buttonsAdministration">
+                <button onclick="redirect('../ajax/common_tabs.php', 'view_roles')" id="view_roles" value="view_roles"><i class="fa fa-user-tag"></i> Roles</button>
+            </div>
+            <div class="buttonsAdministration">
+                <button onclick="redirect('../ajax/common_tabs.php', 'view_sales')" id="view_sales" value="view_sales"><i class="fa fa-shopping-cart"></i> Ventas</button>
+            </div>
+            <div class="buttonsAdministration">
+                <button onclick="redirect('../ajax/common_tabs.php', 'view_orders')" id="view_orders" value="view_orders"><i class="fa fa-shopping-bag"></i> Pedidos</button>
             </div>
         </div>
     </div>
