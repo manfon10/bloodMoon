d6e5e9048f227b08760851eeed7ce5a1b020cb53
Manuel Fonseca <manueldazafon@gmail.com>
2021-04-08 17:24:39 -0500

    Creo clase Security, creo metodos para validaciones de sesion y permios.

--- config/security.php
+++ config/security.php
@@ -1,10 +1,42 @@
 <?php
+    
+    session_start();
 
-if($_SESSION['access'] != 1){
+    require_once('../controllers/RolController.php');
 
-    $error = "Por favor inicie sesion :')";
-    header('Location: ../index.php?redirec=index&msg=' . $error);
-    exit();
-}
+    class Security {
+
+        public $rolController;
+
+            public function __construct() {
+                $this->rolController = new RolController();
+            }
+
+            public function validateAcces(){
+                if($_SESSION['access'] != 1){
+                    $error = "Por favor inicie sesion :')";
+                    header('Location: ../index.php?redirec=index&msg=' . $error);
+                    exit();
+                }
+            }
+
+            public function validatePermissionsUser() {
+                if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    $this->rolController->validatePermissions($_SESSION['rol_user'], $_GET['action'], $_GET['menu']);
+                }else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+                    $this->rolController->validatePermissions($_SESSION['rol_user'], $_GET['action'], $_POST['menu']);
+                }
+            }
+            
+    }
+
+    $security = new Security();
+    $security->validateAcces();
+
+    if(isset($_GET['action'])) {
+        $security->validatePermissionsUser();
+    }else if(isset($_POST['action'])) {
+        $security->validatePermissionsUser();
+    }
 
 ?>
