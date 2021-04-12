6cddbb35f904678305f00c8c3fd98b7bfaa50e04
Manuel Fonseca <manueldazafon@gmail.com>
2021-04-12 13:05:01 -0500

    agrego validacion para traer la vista de roles

--- ajax/common_tabs.php
+++ ajax/common_tabs.php
@@ -1,21 +1,31 @@
 <?php
 
     require_once("../models/administrationModel.php");
+    require_once("../models/rolModel.php");
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $modelo = new administrationModel();
+        $admin_model = new administrationModel();
         if($_POST['data'] == 'view_user'){
-            $data = $modelo->getUser();
+            $data = $admin_model->getUser();
             include_once('../views/administration/view_user.php');
         }else if($_POST['data'] == 'view_category'){
-            $data = $modelo->getCategory();
+            $data = $admin_model->getCategory();
             if($data != []) {
                 include_once('../views/administration/view_category.php');
             }else {
                 $error = "No hay Datos Disponibles.";
                 include_once('../views/administration/view_category.php');
             }
-            
+        }else if($_POST['data'] == 'view_roles') {
+            $rol_model = new RolModel();
+            $rolesArray = $rol_model->getPermissionsByRol();
+            $data = $rolesArray;
+                if($data != []) {
+                    include_once('../views/administration/view_rol.php');
+                }else {
+                    $error = "No hay Datos Disponibles.";
+                    include_once('../views/administration/view_rol.php');
+                }
         }
     }
 
