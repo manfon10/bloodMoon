ef28f1bb154b2c2958d955eae53c62a1f4c87447
Manuel Fonseca <manueldazafon@gmail.com>
2021-04-12 13:03:40 -0500

    agrego archivo view_rol donde creo tabla para mostrar roles y permisos, tambien sus acciones.

---
+++ views/administration/view_rol.php
@@ -0,0 +1,25 @@
+<div class="containerFormsAdministration">
+    <table class="tableProducts" style="width:70%;">
+        <thead class="tableHead">
+            <tr>
+                <th>Nombre Rol</th>
+                <th>Permisos</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody class="tableBody">
+            <?php if($data != []) :?>
+                <?php foreach($data as $dato) :?>
+                    <tr>
+                        <td><?php echo $dato['nombre_rol']; ?></td>
+                        <td><?php echo $dato['Permisos']; ?></td>
+                        <td>
+                            <a href="" ><i class="fa fa-edit"></i></a>
+                            <a href="" ><i class="fa fa-trash-alt"></i></a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</div>
